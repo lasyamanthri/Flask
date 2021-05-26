db17d3bf4f19f1f716c48a829f759fadc3b21a72
lasyamanthri <67892336+lasyamanthri@users.noreply.github.com>
2021-05-26 22:21:53 +0530

    Add files via upload

--- crud.sql
+++ crud.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2018 at 05:51 AM
--- Server version: 5.7.14
--- PHP Version: 7.0.10
+-- Generation Time: May 26, 2021 at 06:47 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,6 +25,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `ip`
+--
+
+CREATE TABLE `ip` (
+  `id` int(11) NOT NULL,
+  `Ip4` int(4) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `students`
 --
 
@@ -30,27 +43,38 @@ CREATE TABLE `students` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL
+  `phone` varchar(255) NOT NULL,
+  `Ip4` int(4) UNSIGNED NOT NULL,
+  `Count` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `name`, `email`, `phone`) VALUES
-(3, 'Parwiz', 'parwiz.f@gmail.com', '009378976767'),
-(4, 'John Doe', 'johndoe@gmail.com', '999999999'),
-(5, 'Karimja', 'ka@gmail.com', '7333392'),
-(6, 'Jamal', 'ja@gmail.com', '3434343'),
-(7, 'Nawid', 'na@gmail.com', '343434'),
-(8, 'Tom Logan', 'Tom@gmail.com', '7347374347'),
-(12, 'Tom Logan', 'tom@gmail.com', '11111111111'),
-(13, 'Fawad', 'fa@gmail.com', '347374837483'),
-(14, 'Wahid', 'wa@gmail.com', '4354354345');
+INSERT INTO `students` (`id`, `name`, `email`, `phone`, `Ip4`, `Count`) VALUES
+(33, 'Lasya', '17h6a1a05@cvsr.ac.in', '1234567890', 0, 0),
+(34, 'Ram ', '17h6a1a05@cvsr.ac.in', '1234567890', 0, 0),
+(35, 'Sai Prasad', '17h6a1a05@cvsr.ac.in', '1234567890', 0, 0),
+(46, 'Lasya', 'lasyamanthri@gmail.com', '1234567890', 0, 0),
+(47, 'Lasya', 'lasyamanthri@gmail.com', '12345', 0, 0),
+(48, 'Ram ', '17h6a1a05f0@cvsr.ac.in', '1234567890', 0, 0),
+(54, 'Lasya', 'lasyamanthri@gmail.com', '1234567890', 0, 0),
+(53, 'Sai Prasad', '17h6a1a05h9@cvsr.ac.in', '12345', 2130706433, 0),
+(52, 'Lasya', 'lasyamanthri@gmail.com', '12345', 2130706433, 0),
+(55, 'Lasya', 'lasyamanthri@gmail.com', '1234567890', 0, 0),
+(56, 'Lasya', 'lasyamanthri@gmail.com', '1234567890', 0, 0),
+(57, 'Lasya', 'lasyamanthri@gmail.com', '1234567890', 0, 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `ip`
+--
+ALTER TABLE `ip`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `students`
@@ -63,10 +87,18 @@ ALTER TABLE `students`
 --
 
 --
+-- AUTO_INCREMENT for table `ip`
+--
+ALTER TABLE `ip`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
